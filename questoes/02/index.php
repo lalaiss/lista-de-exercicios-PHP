27a138644409670c9e572ce4900ba9afe38f56d2
Laís Magalhães <lais.silva13@estudante.ifto.edu.br>
2025-09-23 11:09:24 -0300

    Questão 02
    Resolução da Questão 2 da atividade

--- questoes/02/index.php
+++ questoes/02/index.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF--8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Inicial - Exercícios PHP</title>
     <link rel="stylesheet" href="./../../styles.css">
@@ -15,11 +15,22 @@
     </header>
 
     <main>
-
     <!-- implementação da solução -->
+
+    <form  method = "post">
+         Digite um número: <input type="number" name="metros" required>
+         <input type="submit" value="Enviar">
+    </form>
+    
+     <?php
      
+        $metros= $_POST ['metros'];
+
+        $resultado = $metros * 100;
+ 
+        echo "O valor  $metros metros convertido em centímetros é :" . $resultado = $metros * 100 . "cm";
+     ?>
     </main>
+
 </body>
-
-
 </html>
