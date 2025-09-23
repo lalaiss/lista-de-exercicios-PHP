cd6edbaf01154ccb4f7ccff2ec15a1a33597a608
Laís Magalhães <lais.silva13@estudante.ifto.edu.br>
2025-09-23 11:50:14 -0300

    Questão 03
    Resolução da Questão 03

--- questoes/03/index.php
+++ questoes/03/index.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF--8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Inicial - Exercícios PHP</title>
     <link rel="stylesheet" href="./../../styles.css">
@@ -15,9 +15,23 @@
     </header>
 
     <main>
-
     <!-- implementação da solução -->
-     
+
+    <form  method = "post">
+         Escreva o valor que você ganha por horas trabalhadas: <input type="number" name="horas" required>
+         <br> Escreva a quantidade de horas trabalhadas no mês: <input type="number" name="mes" required>
+         <br> <input type="submit" value="Enviar">
+    </form>
+
+    <?php
+        $horas= $_POST ['horas'];
+        $mes= $_POST ['mes'];
+
+        $resultado = $horas * $mes;
+
+        echo " O salário mensal será de : R$" . $resultado . ",00";
+
+    ?>
     </main>
 </body>
 
