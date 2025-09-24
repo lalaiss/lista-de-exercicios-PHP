ac05fd1405ddba771a89ce0692448a5ab9d1a142
Laís Magalhães <lais.silva13@estudante.ifto.edu.br>
2025-09-24 20:45:47 -0300

    Questão 09
    Resolução da questão 09

--- questoes/09/index.php
+++ questoes/09/index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF--8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página Inicial - Exercícios PHP</title>
+    <title>Questão 09</title>
     <link rel="stylesheet" href="./../../styles.css">
 </head>
 
@@ -15,9 +15,25 @@
     </header>
 
     <main>
-
+ 
     <!-- implementação da solução -->
      
+    <form>
+    <label for="nome">Escreva o seu nome:</label> <input type="text"  name="nome" required>
+    <label for="idade">Escreva o sua idade:</label> <input type="text"  name="idade" required>
+    <button type="submit">Enviar</button>
+    </form>
+     
+    <?php
+     if (isset($_GET['idade'])) {
+        $idade = $_GET['idade'];
+        $nome = $_GET['nome'];
+
+        $calculodias = $idade * 365;
+        
+        echo $nome . " você possui " . $calculodias . " dias de vida. ";
+     }
+    ?>
     </main>
 </body>
 
